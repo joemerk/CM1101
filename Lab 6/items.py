63bac002947c96fe02a21c2432558b93ef931537
joemerk <merkourisj@cardiff.ac.uk>
2017-10-16 14:33:50 +0100

    Added Lab 4 and Lab 6 folder/files

---
+++ Lab 6/items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "mass": 1,
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "mass": 6,
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "mass": 2,
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "mass": 3,
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "mass": 1,
+
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "mass": 7,
+
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously."
+}
---
+++ Lab 6/player.py
@@ -0,0 +1,8 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+total_weight = 0
+max_weight = 15
+# Start game at the reception
+current_room = rooms["Reception"]
